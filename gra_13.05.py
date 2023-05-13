f91a4963b0dc0281a7247b9563d9ec5b8586f26a
Lokkkkiiii <113685091+Lokkkkiiii@users.noreply.github.com>
2023-05-13 18:02:48 +0200

    Update gra_13.05.py

--- gra_13.05.py
+++ gra_13.05.py
@@ -6,56 +6,55 @@ import random
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def print_game_board(player_position, objects_positions, map):
+def mapa(player_x, meteory, map):
     clear_console()
     for row in range(len(map)):
         for col in range(len(map[row])):
-            if row == len(map) - 1 and col == player_position:
+            if row == len(map) - 1 and col == player_x:
                 print('O', end='')
-            elif (row, col) in objects_positions:
+            elif (row, col) in meteory:
                 print('X', end='')
             else:
                 print(' ', end='')
         print()
 
-def update_player_position(player_row, player_position):
-    return (player_row, player_position)
+def update_player(player_y, player_x):
+    return (player_y, player_x)
 
-def update_objects_positions(objects_positions):
-    return [(row + 1, col) for row, col in objects_positions]
+def update_meteory(meteory):
+    return [(row + 1, col) for row, col in meteory]
 
-def create_new_object():
-    return (0, random.randint(0, 7))
+def create_meteory():
+    return (0, random.randint(0, 9))
 
-def check_collision(player_row, player_position, objects_positions):
-    for pos in objects_positions:
-        if  pos[1] == player_position and pos[0] == player_row:
+def check_collision(player_y, player_x, meteory):
+    for pos in meteory:
+        if  pos[1] == player_x and pos[0] == player_y:
             return True
     return False
 
 def game():
-    player_position = 0
-    objects_positions = [create_new_object()]
-    map = [['#']*8 for _ in range(8)]
-    player_row = len(map) - 2
+    player_x = 0
+    meteory = [create_meteory()]
+    map = [[' ']*10 for _ in range(10)]
+    player_y = len(map) - 2
     while True:
-        print_game_board(player_position,objects_positions, map)
-        time.sleep(0.5)
+        mapa(player_x,meteory, map)
+        time.sleep(0.4)
         if msvcrt.kbhit():
             key = msvcrt.getch()
             if key == b'a':
-                player_position = max(0, player_position - 1)
+                player_x = max(0, player_x - 1)
             elif key == b'd':
-                player_position = min(len(map[0]) - 1, player_position + 1)
+                player_x = min(len(map[0]) - 1, player_x + 1)
         if random.random() < 1:
-            objects_positions.append(create_new_object())
-        objects_positions = [pos for pos in objects_positions if pos[0] < len(map) - 1]
-        objects_positions = update_objects_positions(objects_positions)
-        if check_collision(player_row, player_position, objects_positions):
-            print_game_board(player_position, objects_positions, map)
-            print("Game Over!")
-            time.sleep(2)
+            meteory.append(create_meteory())
+        meteory = [pos for pos in meteory if pos[0] < len(map) - 1]
+        meteory = update_meteory(meteory)
+        if check_collision(player_y, player_x, meteory):
+            mapa(player_x, meteory, map)
+            print("Przegrałes")
             break
-        player_row, player_position = update_player_position(player_row, player_position)
+        player_y, player_x = update_player(player_y, player_x)
 
 game()
